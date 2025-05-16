4488f4fd60880306d65642f5e18fd961c5121e56
Petr Knoll <hello@petrknoll.co>
2025-05-16 16:57:19 +0200

    inter

--- src/assets/fontLoader.js
+++ src/assets/fontLoader.js
@@ -6,9 +6,21 @@ import aeonikBold from './aeonik/Aeonik-Bold.woff2';
 import aeonikFonoRegular from './aeonik-fono/AeonikFono-Regular.otf';
 import aeonikFonoMedium from './aeonik-fono/AeonikFono-Medium.otf';
 
+// Import Inter fonts
+import interRegular from './inter/Inter-Regular.woff2';
+import interMedium from './inter/Inter-Medium.woff2';
+import interSemiBold from './inter/Inter-SemiBold.woff2';
+import interBold from './inter/Inter-Bold.woff2';
+
 // Function to set up font CSS variables
 export function setupFonts() {
   const root = document.documentElement;
+  
+  // Set up Inter fonts
+  root.style.setProperty('--font-inter-regular', `url("${interRegular}") format("woff2")`);
+  root.style.setProperty('--font-inter-medium', `url("${interMedium}") format("woff2")`);
+  root.style.setProperty('--font-inter-semibold', `url("${interSemiBold}") format("woff2")`);
+  root.style.setProperty('--font-inter-bold', `url("${interBold}") format("woff2")`);
   
   // Set up Aeonik fonts
   root.style.setProperty('--font-aeonik-regular', `url("${aeonikRegular}") format("woff2")`);
